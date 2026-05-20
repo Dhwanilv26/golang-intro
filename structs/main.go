d728e3c55833585c21de3ad28203d7c139530951
Dhwanil Vaidya <166910831+Dhwanilv26@users.noreply.github.com>
2026-05-20 09:23:02 +0000

    started with embedding structs

--- structs/main.go
+++ structs/main.go
@@ -2,15 +2,40 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	// go assigns 0 / default values on empty structs 
-	var alexp person
-	alex := person{firstName: "alex", lastName: "anderson"}
-	fmt.Printf("%+v",alexp)
-	fmt.Printf("%+v",alex)
+	// go assigns 0 / default values on empty structs
+	// var alexp person
+	// alex := person{firstName: "alex", lastName: "anderson"}
+	// fmt.Printf("%+v", alexp)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "abc@gmail.com",
+			zipCode: 360242,
+		},
+	}
+	jim.updateName("jimmy")
+	jim.print()
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
