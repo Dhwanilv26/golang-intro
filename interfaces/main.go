67087279641349769246ed2da86468fe5c06d107
Dhwanilv26 <dhwanil2604@gmail.com>
2026-05-21 19:20:32 +0530

    basic interfaces done, moving to http requests

--- interfaces/main.go
+++ interfaces/main.go
@@ -2,9 +2,19 @@ package main
 
 import "fmt"
 
-type englishBot struct{}
+type bot interface { // interface type
+	getGreeting() string
+}
+
+// getGreeting(string) string , not possible inside go, (go doesnt support method overloading), ofc not supported in structs too, and not supported in interfaces as well
+
+type englishBot struct{} // concrete type
 type spanishBot struct{}
 
+// struct has to satisfy all methods of an interface to be considered that interface,
+// interfaces in go can only have methods and not variables
+// cant initialize or create any value of the interface types, can only do so from the concrete types (all go types like string, int and then the custom ones like englishbot) , interfaces in go are ofc abstract thats why
+// interfaces are always implicit in go, we dont write englishbot 'satifies' bot interface
 func main() {
 
 	eb := englishBot{}
@@ -15,13 +25,8 @@ func main() {
 
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
-}
-
-// in go, there is no support for method overloading, so cant have same function names in the same package
-func printGreeting(sb spanishBot) {
-	fmt.Println(sb.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
 func (eb englishBot) getGreeting() string {
